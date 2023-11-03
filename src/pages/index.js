526773ac3bc8b42945592bb599502be8e4aab0de
Matheus Hiroyuki Suwa Moura <mmoura@nelogica.com.br>
2023-11-03 12:01:17 -0300

    Adicionando banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,14 @@ import Formulario from "./components/Formulario/formulario"
 import Footer from "./components/Footer/footer"
 import Pokemon from "./components/Pokemon/pokemon"
 import Iniciais from "./components/Iniciais/iniciais"
+import Header from '../../components/Header'
+import Banner from '../../components/Banner'
 
 export default function Home() {
   return (
     <>
+      <Header />
+      <Banner />
       <Iniciais />
       <Formulario />
       <Footer />
