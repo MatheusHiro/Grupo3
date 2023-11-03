a19b15ea656e0cb032d625eac0cfca75d26d47ea
claratheisen <kiwib1r9@gmail.com>
2023-11-02 21:43:16 -0300

    home pronta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
+import Header from '../../components/Header'
+import Banner from '../../components/Banner'
+//import styles from '../styles/Home.module.css'
+import localFont from 'next/font/local'
+
+
+const myFont = localFont({
+  src: '../../public/fonts/uni05_53.ttf'
+}
+)  
+
 export default function Home() {
   return (
-    <h1>
-      Olá, mundo!
-    </h1>
+    <div className={myFont.className}>
+      <Header />
+      <Banner />
+    </div>
+    
+
+    
+
+    
+
   )
 }
---
+++ src/pages/iniciais.js
@@ -0,0 +1,7 @@
+export default function Iniciais() {
+    return (
+
+        <h1>Iniciais</h1>
+
+    )
+  }
