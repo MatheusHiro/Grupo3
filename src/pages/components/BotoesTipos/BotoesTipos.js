4690a9756b19891598d939eb56f34abc4cf97295
Alef-Pasqualotto <pasqualotto.alef@gmail.com>
2023-11-04 12:49:57 -0300

    cores lindas e coloridas funcionando

--- src/pages/components/BotoesTipos/BotoesTipos.js
+++ src/pages/components/BotoesTipos/BotoesTipos.js
@@ -6,7 +6,7 @@ import Agua from "@img/agua";
 import border from '../../../styles/borders.module.css'
 import { useState } from "react";
 
-function ColoredImage({ ImageComponent, fillColor, borderClass, type, hideText }) {
+function ColoredImage({ ImageComponent, fillColor, borderClass, type, hideText, style }) {
     return (
         <div className={styles.tipos_text}>
             {/* Condição para tirar o texto quando vai para a tela dos pokemons */}
@@ -16,7 +16,7 @@ function ColoredImage({ ImageComponent, fillColor, borderClass, type, hideText }
                 </p>
             )}
             <div className={styles.elements}>
-                <button className={`${borderClass} ${border.pixelCorners}`}>
+                <button style={style} className={`${borderClass} ${border.pixelCorners}`}>
                     <ImageComponent fill={fillColor} />
                 </button>
             </div>
@@ -25,6 +25,43 @@ function ColoredImage({ ImageComponent, fillColor, borderClass, type, hideText }
 }
 
 export default function BotoesTipos(props) {
+
+    function clicaBotao(tipo) {
+        if (tipo == 'AGUA') {
+            setStylebtn1({ backgroundColor: '#3B6388' })
+            setStylebtn2({ backgroundColor: 'white' })
+            setStylebtn3({ backgroundColor: 'white' })
+
+            setImgbt1('white')
+            setImgbt2('#3B6388')
+            setImgbt3('#3B6388')
+        } else if (tipo == 'PLANTA') {
+            setStylebtn1({ backgroundColor: 'white' })
+            setStylebtn2({ backgroundColor: '#3B8863' })
+            setStylebtn3({ backgroundColor: 'white' })
+
+            setImgbt1('#3B8863')
+            setImgbt2('white')
+            setImgbt3('#3B8863')
+        } else {
+            setStylebtn1({ backgroundColor: 'white' })
+            setStylebtn2({ backgroundColor: 'white' })
+            setStylebtn3({ backgroundColor: '#AD3F3F' })
+
+            setImgbt1('#AD3F3F')
+            setImgbt2('#AD3F3F')
+            setImgbt3('white')
+        }
+    }
+
+    const [corBt1, setImgbt1] = useState('#603B88');
+    const [corBt2, setImgbt2] = useState('#603B88');
+    const [corBt3, setImgbt3] = useState('#603B88');
+
+    const [stylebtn1, setStylebtn1] = useState({ backgroundColor: 'white' });
+    const [stylebtn2, setStylebtn2] = useState({ backgroundColor: 'white' });
+    const [stylebtn3, setStylebtn3] = useState({ backgroundColor: 'white' });
+
     const [hideText, setHideText] = useState(false);
 
     const mudaTipo = (tipo) => {
@@ -36,11 +73,12 @@ export default function BotoesTipos(props) {
             <div className={styles.move}>
                 <div onClick={() => {
                     props.funcAgua();
-                    mudaTipo('AGUA');
+                    clicaBotao('AGUA')
                 }}>
                     <ColoredImage
+                        style={stylebtn1}
                         ImageComponent={Agua}
-                        fillColor={props.fillColor}
+                        fillColor={corBt1}
                         borderClass={props.borderClass}
                         type={"AGUA"}
                         hideText={hideText}
@@ -50,11 +88,12 @@ export default function BotoesTipos(props) {
             <div className={styles.move}>
                 <div onClick={() => {
                     props.funcPlanta();
-                    mudaTipo('PLANTA');
+                    clicaBotao('PLANTA')
                 }}>
                     <ColoredImage
+                        style={stylebtn2}
                         ImageComponent={Planta}
-                        fillColor={props.fillColor}
+                        fillColor={corBt2}
                         borderClass={props.borderClass}
                         type={"PLANTA"}
                         hideText={hideText}
@@ -64,11 +103,12 @@ export default function BotoesTipos(props) {
             <div className={styles.move}>
                 <div onClick={() => {
                     props.funcFogo();
-                    mudaTipo('FOGO');
+                    clicaBotao('FOGO')
                 }}>
                     <ColoredImage
+                        style={stylebtn3}
                         ImageComponent={Fogo}
-                        fillColor={props.fillColor}
+                        fillColor={corBt3}
                         borderClass={props.borderClass}
                         type={"FOGO"}
                         hideText={hideText}
