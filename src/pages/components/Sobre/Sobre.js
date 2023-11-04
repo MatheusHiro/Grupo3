35294354f6cc38768775f318a9a64368d0833e11
claratheisen <kiwib1r9@gmail.com>
2023-11-04 12:38:31 -0300

    alterações clara

--- src/pages/components/Sobre/Sobre.js
+++ src/pages/components/Sobre/Sobre.js
@@ -1,14 +1,34 @@
 import styles from './sobre.module.css'
+import Image from 'next/image'
+import nekoComemorando from '../../../../public/img/neko-comemorando.png'
+import logo from '../../../../public/img/logo-pixelado-branco.png'
 
 export default function Sobre() {
     return (
-        <div className={styles.sobre}>
-            <p>SOBRE NOS</p>
-            <p>
-                A IDE é a empresa júnior dos cursos Ciência e Engenharia de Computação da Universidade Federal do Rio Grande do Sul. Formada por alunos interessados em se tornarem empreendedores e profissionais excepcionais, a IDE fornece soluções em software, como sites e sistemas, com preços abaixo do mercado, aliados à excelência da UFRGS, o Instituto de Informática e seus professores, que fornecem apoio técnico e aconselhamento aos estudantes.
-            </p>
-        </div>
-    );
+        <body className={styles.sobre}>
+            <div>
+                <div>
+                    <h2 className={styles.sobrenos}>
+                        SOBRE NOS
+                    </h2>
+                    <div className={styles.texto_container}>
+                        <p className={styles.letras}>
+                            A IDE é a empresa júnior dos cursos Ciência e Engenharia de Computação da Universidade Federal do Rio Grande do Sul. Formada por alunos interessados em se tornarem empreendedores e profissionais excepcionais, a IDE fornece soluções em software, como sites e sistemas, com preços abaixo do mercado, aliados à excelência da UFRGS, o Instituto de Informática e seus professores, que fornecem apoio técnico e aconselhamento aos estudantes.
+                            <p></p>
+                            <Image src={logo} width={50}/>
+                        </p>
+                    </div>
+                </div> 
+
+                <div className={styles.neko}>
+                    <Image src={nekoComemorando} width={300}/>
+                </div>
+                
+            </div>
+
+            
+        </body>
+    )
 }
 
 /*
@@ -28,3 +48,4 @@ export default function Sobre() {
          href="globals.css">
 </head>
 */
+
