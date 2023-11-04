a14ea703abef7e7a6bda833da8901aef0df58fab
Matheus Hiroyuki Suwa Moura <mmoura@nelogica.com.br>
2023-11-04 14:08:48 -0300

    Salvando o trab

--- src/pages/components/Sobre/Sobre.js
+++ src/pages/components/Sobre/Sobre.js
@@ -3,30 +3,30 @@ import styles from './sobre.module.css'
 export default function Sobre() {
     return (
         <div className={styles.sobre}>
-            <div className={styles.container}>
-                <div>
-                    <h2 className={styles.sobrenos}>
-                        SOBRE NOS
-                    </h2>
-                    <div className={styles.texto_container}>
-                        <p className={styles.letras}>
-                            A IDE é a empresa júnior dos cursos Ciência e Engenharia de Computação da Universidade Federal do Rio Grande do Sul. Formada por alunos interessados em se tornarem empreendedores e profissionais excepcionais, a IDE fornece soluções em software, como sites e sistemas, com preços abaixo do mercado, aliados à excelência da UFRGS, o Instituto de Informática e seus professores, que fornecem apoio técnico e aconselhamento aos estudantes.
-                            <p></p>
-                            <img src='img/logo-pixelado-branco.png' className={styles.ide} />
-                        </p>
-                    </div>
-                </div>
+            <div className={styles.alinhar}>
+                <p className={styles.sobrenos}>SOBRE NOS</p>
+                <div className={styles.textobox} >
+                    <p className={styles.letras}>
+                        A IDE é a empresa júnior dos cursos Ciência e
+                        Engenharia de Computação da Universidade
+                        Federal do Rio Grande do Sul. Formado por alunos
+                        interessados em se tornarem empreendedores a
+                        profissionais excepcionais, a IDE fornece soluções
+                        em software, como sites e sistemas, com preços
+                        abaixo do mercado, aliados à excelência da
+                        UFRGS, o Instituto de Informática e seus
+                        professores, que fornecem apoio técnico e
+                        aconselhamento aos estudantes.
+                    </p>
+                    <img src='img/logo-pixelado-branco.png' className={styles.ide} />
 
-                <div className={styles.neko}>
-                    <img src='img/neko-comemorando.png' className={styles.neko} />
                 </div>
-
             </div>
-
-
+            <div>
+                <img src='img/neko-comemorando.png' className={styles.neko} />
+            </div>
         </div>
-    )
+
+
+    );
 }
-
-
-
